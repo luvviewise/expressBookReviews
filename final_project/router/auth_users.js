d0a4be7c60465b7cd600c2805b3e3da683867905
Luvvie Wise <vwise36@gmail.com>
2026-08-26 15:31:14 -0400

    Update changes/ Added a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,9 +9,9 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 //Check if the username already exists in the users array
 let userswithssamename = user.filter((user) =>{
-    return (user.username===username && user.password === password);
+    return (user.username === username && user.password === password);
 });
-return validusers.length > 0;
+return userswithssamename.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
@@ -30,7 +30,7 @@ regd_users.post("/login", (req,res) => {
   const {username, password} =req.body;
 
   if (!username || !password) {
-    return res.status(404). json({ message:"Error logging in. Provide username and password." });
+    return res.status(403). json({ message:"Error logging in. Provide username and password." });
   }
 
   if (authenticatedUser(username, password)) {
@@ -44,17 +44,33 @@ regd_users.post("/login", (req,res) => {
         accessToken, username
     };
 
-    return res.status(200).send("User sucessful loggedin");
+    return res.status(200).send("User successful loggedin");
 } else {
     
-    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    return res.status(403).json({ message: "Invalid Login. Check username and password" });
 }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.query.review; // note or req.body.review
+  const username = req.user.username; //extracted from verified JWT token 
+
+  if (!review) {
+    return res.status(400).json({message: "Review content is required"});
+  }
+
+  //Check if the book exists in your database
+  if (book[isbn])  {
+    //Add or update the review under the current user's username
+    books[isbn] ["reviews"] [username] = review;
+    return res.status(200).json ({ message: 'Review for ISBN ${isbn} successfully added/updated.'});
+  } else {
+    return res.status (404).json({ message: "Books not found" });
+  }
+
 });
 
 module.exports.authenticated = regd_users;
